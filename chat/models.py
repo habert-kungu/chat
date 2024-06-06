82dc42efe2f0437cd8767c0337edb98d063a5814
habert <habert231@gmail.com>
2024-06-06 15:10:36 +0300

    configuring room models

--- chat/models.py
+++ chat/models.py
@@ -19,4 +19,27 @@ class Message(models.Model):
 
 
 class Room(models.Model):
-    pass
+    WAITING = "waiting"
+    ACTIVE = "active"
+    CLOSED = "closed"
+
+    CHOICES_STATUS = (
+        (WAITING, "Waiting"),
+        (ACTIVE, "Active"),
+        (CLOSED, "Closed"),
+    )
+    uuid = models.CharField(max_length=255)
+    client = models.CharField(max_length=255)
+    agent = models.ForeignKey(
+        User, related_name="rooms", blank=True, null=True, on_delete=models.SET_NULL
+    )
+    messages = models.ManyToManyField(Message, blank=True)
+    url = models.CharField(max_length=255, blank=True, null=True)
+    status = models.CharField(max_length=20, choices=CHOICES_STATUS, default=WAITING)
+    created_at = models.DateTimeField(auto_now_add=True)  # Corrected field name
+
+    class Meta:
+        ordering = ("-created_at",)
+
+    def __str__(self):
+        return f"{self.client} - {self.uuid}"
