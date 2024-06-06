5df71f8b23d7cd4d511f6a2d20f265990e401b76
habert <habert231@gmail.com>
2024-06-06 16:53:57 +0300

    adding and styling chat bubble 1

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,25 @@
+//Elements
 
+const chatElement = document.querySelector("#chat");
+const chatOpenElement = document.querySelector("#chat_open");
+const chatJoinElement = document.querySelector("#chat_join");
+const chatIconElement = document.querySelector("#chat_icon");
+const chatWelcomeElement = document.querySelector("#chat_welcome");
+const chatRoomElement = document.querySelector("#chat_room");
+
+//Event listeners
+chatOpenElement.onclick = function(e) {
+  e.preventDefault();
+  chatIconElement.classList.add("hidden");
+  chatWelcomeElement.classList.remove("hidden");
+
+  return false;
+};
+
+chatJoinElement.onclick = function(e) {
+  e.preventDefault();
+  chatWelcomeElement.classList.add("hidden");
+  chatRoomElement.classList.remove("hidden");
+
+  return false;
+};
