f7d9264c1b8e4951fb7079082991f7f7a8f8f933
habert <habert231@gmail.com>
2024-06-10 08:38:25 +0300

    setting up end point for creating chat room

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,15 @@
+import json
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
+from .models import Room
 
-# Create your views here.
+
+@require_POST
+def create_room(request, uuid):
+    name = request.POST.get("name", "")
+    url = request.POST.get("url", "")
+
+    Room.objects.create(uuid=uuid, client=name, url=url)
+
+    return JsonResponse({"message": "Room Created"})
